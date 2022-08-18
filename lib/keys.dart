47cb1781c978505276a249cf7c2ad5e50162a5e1
jinosh05 <jinoshprabhuraj5@gmail.com>
2022-08-18 23:27:51 +0530

    Key File created

---
+++ lib/keys.dart
@@ -0,0 +1,11 @@
+class Keys {
+  static final String stringKey = "string_key";
+
+  static final String boolKey = "bool_key";
+
+  static final String listKey = "ls_key";
+
+  static final String doubleKey = "double_key";
+
+  static final String intKey = "int_key";
+}
